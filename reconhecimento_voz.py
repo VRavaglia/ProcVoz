6a595917fedbd6211c152a2e212adf60bce70f8e
Bernardo Boechat <bernardoant@poli.ufrj.br>
2021-10-10 12:02:56 -0300

    Text-to-speech implementado com a API da Google.

--- reconhecimento_voz.py
+++ reconhecimento_voz.py
@@ -1,4 +1,6 @@
-import speech_recognition as speech_recognition
+import speech_recognition as sr
+import os
+from google.cloud import texttospeech_v1
 from gtts import gTTS
 from playsound import playsound
 
@@ -16,7 +18,26 @@ def ouvir_microfone():
     return posicao
 
 def criar_audio(texto):
-    tts = gTTS(texto, lang='pt-br')
-    tts.save('audios/cpu_move.wav')
-    playsound('audios/cpu_move.wav')
 
+    # Set credentials environment variable and initialize text to speech client.
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "batalha-naval-328614-3953378a20b8.json"
+    cliente = texttospeech_v1.TextToSpeechClient()
+
+    # Specify voice and configuration parameters for Google TTS.
+    voz = texttospeech_v1.VoiceSelectionParams(
+        language_code='pt-BR',
+        name='pt-BR-Wavenet-A'
+    )
+    audio_config = texttospeech_v1.AudioConfig(
+        audio_encoding=texttospeech_v1.AudioEncoding.MP3
+    )
+
+    # Synthetize sentence recording.
+    resposta = cliente.synthesize_speech(
+        request={"input": texttospeech_v1.SynthesisInput(text=texto), "voice": voz, "audio_config": audio_config}
+    )
+
+    # Save audio file.
+    with open(r"voz.mp3", "wb") as saida:
+        saida.write(resposta.audio_content)
+    playsound("voz.mp3")
